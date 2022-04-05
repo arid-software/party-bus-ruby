fcf7963e8ce249f57b89ad3c3cb90d8f927edc33
corey jergensen <corey@aridsoftware.com>
2022-04-05 12:15:13 -0500

    Added sources and sub-entities.

--- lib/party_bus/events/create.rb
+++ lib/party_bus/events/create.rb
@@ -1,14 +1,16 @@
 module PartyBus
   module Events
     class Create < PartyBus::Base
-      def self.perform_using(resource_type:, resource_action:, payload:)
-        new(resource_type, resource_action, payload).perform
+      def self.perform_using(payload:, resource_type:, resource_action:, source_id:, sub_entity_id:)
+        new(resource_type, resource_action, payload, source_id, sub_entity_id).perform
       end
 
-      def initialize(resource_type, resource_action, payload)
+      def initialize(resource_type, resource_action, payload, source_id, sub_entity_id)
         @resource_type = resource_type
         @resource_action = resource_action
         @payload = payload
+        @source_id = source_id
+        @sub_entity_id = sub_entity_id
       end
 
       private
@@ -27,9 +29,11 @@ module PartyBus
       def body
         {
           event: {
+            payload: @payload,
             resource_type: @resource_type,
             resource_action: @resource_action,
-            payload: @payload
+            source_id: @source_id,
+            sub_entity_id: @sub_entity_id
           }
         }
       end
